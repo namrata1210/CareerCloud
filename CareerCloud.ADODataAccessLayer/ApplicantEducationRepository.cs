3d1c1393d2706db95dd05e3ace33f7ce38f7214c
jigar <jigar@JIGAR>
2018-06-06 15:59:33 -0400

    create base ado and application edducation repository class

--- CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -2,6 +2,7 @@
 using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -9,11 +10,41 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    class ApplicantEducationRepository : IDataRepository<ApplicantEducationPoco>
+    class ApplicantEducationRepository : BaseADO,IDataRepository<ApplicantEducationPoco>
     {
         public void Add(params ApplicantEducationPoco[] items)
         {
-            throw new NotImplementedException();
+            using (_connection)
+            {
+                SqlCommand cmd = new SqlCommand();
+               cmd.Connection = _connection;
+               int  rowseffected = 0;
+
+                foreach(ApplicantEducationPoco poco in items)
+                {
+                    cmd.CommandText = @"INSERT INTO [dbo].[Applicant_Educations] 
+                   ([Id],[Applicant],[Major],[Certificate_Diploma],[Start_Date] ,[Completion_Date] ,[Completion_Percent])
+                   Values(@Id,@Applicant,@Major,@Certificate_Diploma,@Start_Date,@Completion_Date ,@Completion_Percent)";
+
+                    cmd.Parameters.AddWithValue("@ID", poco.Id);
+                    cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                    cmd.Parameters.AddWithValue("@Major", poco.Major);
+                    cmd.Parameters.AddWithValue("@Certificate_Diploma",poco.CertificateDiploma);
+                    cmd.Parameters.AddWithValue("@Start_Date", poco.StartDate);
+                    cmd.Parameters.AddWithValue("@Completion_Date", poco.CompletionDate);
+                    cmd.Parameters.AddWithValue("@Completion_Percent", poco.CompletionDate);
+
+                    _connection.Open();
+                     rowseffected += cmd.ExecuteNonQuery();
+                    _connection.Close();
+                   
+                
+
+
+
+                }
+
+            }
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -45,6 +76,5 @@ namespace CareerCloud.ADODataAccessLayer
         {
             throw new NotImplementedException();
         }
-
     }
 }
