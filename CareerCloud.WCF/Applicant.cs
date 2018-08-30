d01a8bbf5c25ff824e06c58349d5fa8da7815712
jigar <jigar@JIGAR>
2018-08-30 16:32:53 -0400

    create IApplicatiomn Icompany application and company class

---
+++ CareerCloud.WCF/Applicant.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+
+namespace CareerCloud.WCF
+{
+    class Applicant : IApplicant
+       
+    {
+       
+        public void AddApplicantEducation(ApplicantEducationPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantEducationPoco> repo = new EFGenericRepository<ApplicantEducationPoco>(false);
+            ApplicantEducationLogic logic = new ApplicantEducationLogic(repo);
+            logic.Add(pocos);
+        }
+
+        public void AddApplicantJobApplication(ApplicantJobApplicationPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantJobApplicationPoco> repo = new EFGenericRepository<ApplicantJobApplicationPoco>(false);
+            ApplicantJobApplicationLogic logic = new ApplicantJobApplicationLogic(repo);
+            logic.Add(pocos);
+        }
+
+        public void AddApplicantProfile(ApplicantProfilePoco[] pocos)
+        {
+            EFGenericRepository<ApplicantProfilePoco> repo = new EFGenericRepository<ApplicantProfilePoco>(false);
+            ApplicantProfileLogic logic = new ApplicantProfileLogic(repo);
+            logic.Add(pocos);
+
+        }
+
+        public void AddApplicantResume(ApplicantResumePoco[] pocos)
+        {
+            EFGenericRepository<ApplicantResumePoco> repo = new EFGenericRepository<ApplicantResumePoco>(false);
+            ApplicantResumeLogic logic = new ApplicantResumeLogic(repo);
+            logic.Add(pocos);
+        }
+
+        public void AddApplicantSkill(ApplicantSkillPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantSkillPoco> repo = new EFGenericRepository<ApplicantSkillPoco>(false);
+            ApplicantSkillLogic logic = new ApplicantSkillLogic(repo);
+            logic.Add(pocos);
+        }
+
+        public void AddApplicantWorkHistory(ApplicantWorkHistoryPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantWorkHistoryPoco> repo = new EFGenericRepository<ApplicantWorkHistoryPoco>();
+            ApplicantWorkHistoryLogic logic = new ApplicantWorkHistoryLogic(repo);
+            logic.Add(pocos);
+        }
+
+        public List<ApplicantEducationPoco> GetAllApplicantEducation()
+        {
+            EFGenericRepository<ApplicantEducationPoco> repo = new EFGenericRepository<ApplicantEducationPoco>(false);
+            ApplicantEducationLogic logic = new ApplicantEducationLogic(repo);
+           return logic.GetAll();
+        }
+
+        public List<ApplicantJobApplicationPoco> GetAllApplicantJobApplication()
+        {
+
+            EFGenericRepository<ApplicantJobApplicationPoco> repo = new EFGenericRepository<ApplicantJobApplicationPoco>(false);
+            ApplicantJobApplicationLogic logic = new ApplicantJobApplicationLogic(repo);
+            return logic.GetAll();
+        }
+
+        public List<ApplicantProfilePoco> GetAllApplicantProfile()
+        {
+
+            EFGenericRepository<ApplicantProfilePoco> repo = new EFGenericRepository<ApplicantProfilePoco>(false);
+            ApplicantProfileLogic logic = new ApplicantProfileLogic(repo);
+            return logic.GetAll();
+        }
+
+        public List<ApplicantResumePoco> GetAllApplicantResume()
+        {
+            EFGenericRepository<ApplicantResumePoco> repo = new EFGenericRepository<ApplicantResumePoco>(false);
+            ApplicantResumeLogic logic = new ApplicantResumeLogic(repo);
+            return logic.GetAll();
+        }
+
+        public List<ApplicantSkillPoco> GetAllApplicantSkill()
+        {
+            EFGenericRepository<ApplicantSkillPoco> repo = new EFGenericRepository<ApplicantSkillPoco>(false);
+            ApplicantSkillLogic logic = new ApplicantSkillLogic(repo);
+            return logic.GetAll();
+        }
+
+        public List<ApplicantWorkHistoryPoco> GetAllAppliccantWorkHistory()
+        {
+
+            EFGenericRepository<ApplicantWorkHistoryPoco> repo = new EFGenericRepository<ApplicantWorkHistoryPoco>();
+            ApplicantWorkHistoryLogic logic = new ApplicantWorkHistoryLogic(repo);
+            return logic.GetAll();
+        }
+
+        public ApplicantEducationPoco GetSingleApplicantEducation(Guid Id)
+        {
+            EFGenericRepository<ApplicantEducationPoco> repo = new EFGenericRepository<ApplicantEducationPoco>(false);
+            ApplicantEducationLogic logic = new ApplicantEducationLogic(repo);
+            return logic.Get(Id);
+        }
+
+        public ApplicantJobApplicationPoco GetSingleApplicantJobApplication(Guid Id)
+        {
+            EFGenericRepository<ApplicantJobApplicationPoco> repo = new EFGenericRepository<ApplicantJobApplicationPoco>(false);
+            ApplicantJobApplicationLogic logic = new ApplicantJobApplicationLogic(repo);
+            return logic.Get(Id);
+        }
+
+        public ApplicantProfilePoco GetSingleApplicantProfile(Guid Id)
+        {
+            EFGenericRepository<ApplicantProfilePoco> repo = new EFGenericRepository<ApplicantProfilePoco>(false);
+            ApplicantProfileLogic logic = new ApplicantProfileLogic(repo);
+            return logic.Get(Id);
+        }
+
+        public ApplicantResumePoco GetSingleApplicantResume(Guid Id)
+        {
+            EFGenericRepository<ApplicantResumePoco> repo = new EFGenericRepository<ApplicantResumePoco>(false);
+            ApplicantResumeLogic logic = new ApplicantResumeLogic(repo);
+            return logic.Get(Id);
+        }
+
+        public ApplicantSkillPoco GetSingleApplicantSkill(Guid Id)
+        {
+            EFGenericRepository<ApplicantSkillPoco> repo = new EFGenericRepository<ApplicantSkillPoco>(false);
+            ApplicantSkillLogic logic = new ApplicantSkillLogic(repo);
+            return logic.Get(Id);
+        }
+
+        public ApplicantWorkHistoryPoco GetSingleApplicantWorkHistory(Guid Id)
+        {
+
+            EFGenericRepository<ApplicantWorkHistoryPoco> repo = new EFGenericRepository<ApplicantWorkHistoryPoco>();
+            ApplicantWorkHistoryLogic logic = new ApplicantWorkHistoryLogic(repo);
+            return logic.Get(Id);
+        }
+
+        public void RemoveApplicantEducation(ApplicantEducationPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantEducationPoco> repo = new EFGenericRepository<ApplicantEducationPoco>(false);
+            ApplicantEducationLogic logic = new ApplicantEducationLogic(repo);
+            logic.Delete(pocos);
+        }
+
+        public void RemoveApplicantJobApplication(ApplicantJobApplicationPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantJobApplicationPoco> repo = new EFGenericRepository<ApplicantJobApplicationPoco>(false);
+            ApplicantJobApplicationLogic logic = new ApplicantJobApplicationLogic(repo);
+            logic.Delete(pocos);
+        }
+
+        public void RemoveApplicantProfile(ApplicantProfilePoco[] pocos)
+        {
+
+            EFGenericRepository<ApplicantProfilePoco> repo = new EFGenericRepository<ApplicantProfilePoco>(false);
+            ApplicantProfileLogic logic = new ApplicantProfileLogic(repo);
+            logic.Delete(pocos);
+        }
+
+        public void RemoveApplicantResume(ApplicantResumePoco[] pocos)
+        {
+
+            EFGenericRepository<ApplicantResumePoco> repo = new EFGenericRepository<ApplicantResumePoco>(false);
+            ApplicantResumeLogic logic = new ApplicantResumeLogic(repo);
+            logic.Delete(pocos);
+        }
+
+        public void RemoveApplicantSkill(ApplicantSkillPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantSkillPoco> repo = new EFGenericRepository<ApplicantSkillPoco>(false);
+            ApplicantSkillLogic logic = new ApplicantSkillLogic(repo);
+            logic.Delete(pocos);
+        }
+
+        public void RemoveApplicantWorkHistory(ApplicantWorkHistoryPoco[] pocos)
+        {
+
+            EFGenericRepository<ApplicantWorkHistoryPoco> repo = new EFGenericRepository<ApplicantWorkHistoryPoco>();
+            ApplicantWorkHistoryLogic logic = new ApplicantWorkHistoryLogic(repo);
+            logic.Delete(pocos);
+        }
+
+        public void UpdateApplicantEducation(ApplicantEducationPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantEducationPoco> repo = new EFGenericRepository<ApplicantEducationPoco>(false);
+            ApplicantEducationLogic logic = new ApplicantEducationLogic(repo);
+            logic.Update(pocos);
+        }
+
+        public void UpdateApplicantJobApplication(ApplicantJobApplicationPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantJobApplicationPoco> repo = new EFGenericRepository<ApplicantJobApplicationPoco>(false);
+            ApplicantJobApplicationLogic logic = new ApplicantJobApplicationLogic(repo);
+            logic.Update(pocos);
+        }
+
+        public void UpdateApplicantProfile(ApplicantProfilePoco[] pocos)
+        {
+
+            EFGenericRepository<ApplicantProfilePoco> repo = new EFGenericRepository<ApplicantProfilePoco>(false);
+            ApplicantProfileLogic logic = new ApplicantProfileLogic(repo);
+            logic.Update(pocos);
+        }
+
+        public void UpdateApplicantResume(ApplicantResumePoco[] pocos)
+        {
+
+            EFGenericRepository<ApplicantResumePoco> repo = new EFGenericRepository<ApplicantResumePoco>(false);
+            ApplicantResumeLogic logic = new ApplicantResumeLogic(repo);
+            logic.Update(pocos);
+         
+        }
+
+        public void UpdateApplicantSkill(ApplicantSkillPoco[] pocos)
+        {
+            EFGenericRepository<ApplicantSkillPoco> repo = new EFGenericRepository<ApplicantSkillPoco>(false);
+            ApplicantSkillLogic logic = new ApplicantSkillLogic(repo);
+            logic.Update(pocos);
+        }
+
+        public void UpdateApplicantWorkHIstory(ApplicantWorkHistoryPoco[] pocos)
+        {
+
+            EFGenericRepository<ApplicantWorkHistoryPoco> repo = new EFGenericRepository<ApplicantWorkHistoryPoco>();
+            ApplicantWorkHistoryLogic logic = new ApplicantWorkHistoryLogic(repo);
+            logic.Update(pocos);
+        }
+    }
+}
---
+++ CareerCloud.WCF/IApplicant.cs
@@ -0,0 +1,75 @@
+﻿using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.WCF
+{
+    interface IApplicant
+    {
+        [OperationContract]
+        void AddApplicantEducation(ApplicantEducationPoco[] pocos);
+        [OperationContract]
+        List<ApplicantEducationPoco> GetAllApplicantEducation();
+        [OperationContract]
+        ApplicantEducationPoco GetSingleApplicantEducation(Guid Id);
+        [OperationContract]
+        void RemoveApplicantEducation(ApplicantEducationPoco[] pocos);
+        [OperationContract]
+        void UpdateApplicantEducation(ApplicantEducationPoco[] pocos);
+        [OperationContract]
+        void AddApplicantJobApplication(ApplicantJobApplicationPoco[] pocos);
+        [OperationContract]
+        List<ApplicantJobApplicationPoco> GetAllApplicantJobApplication();
+        [OperationContract]
+        ApplicantJobApplicationPoco GetSingleApplicantJobApplication(Guid Id);
+        [OperationContract]
+        void RemoveApplicantJobApplication(ApplicantJobApplicationPoco[] pocos);
+        [OperationContract]
+        void UpdateApplicantJobApplication(ApplicantJobApplicationPoco[] pocos);
+        [OperationContract]
+        void AddApplicantProfile(ApplicantProfilePoco[] pocos);
+        [OperationContract]
+        List<ApplicantProfilePoco> GetAllApplicantProfile();
+        [OperationContract]
+        ApplicantProfilePoco GetSingleApplicantProfile(Guid Id);
+        [OperationContract]
+        void RemoveApplicantProfile(ApplicantProfilePoco[] pocos);
+        [OperationContract]
+        void UpdateApplicantProfile(ApplicantProfilePoco[] pocos);
+        [OperationContract]
+        void AddApplicantResume(ApplicantResumePoco[] pocos);
+        [OperationContract]
+        List<ApplicantResumePoco> GetAllApplicantResume();
+        [OperationContract]
+        ApplicantResumePoco GetSingleApplicantResume(Guid Id);
+        [OperationContract]
+        void RemoveApplicantResume(ApplicantResumePoco[] pocos);
+        [OperationContract]
+        void UpdateApplicantResume(ApplicantResumePoco[] pocos);
+        [OperationContract]
+        void AddApplicantSkill(ApplicantSkillPoco[] pocos);
+        [OperationContract]
+        List<ApplicantSkillPoco> GetAllApplicantSkill();
+        [OperationContract]
+        ApplicantSkillPoco GetSingleApplicantSkill(Guid Id);
+        [OperationContract]
+        void RemoveApplicantSkill(ApplicantSkillPoco[] pocos);
+        [OperationContract]
+        void UpdateApplicantSkill(ApplicantSkillPoco[] pocos);
+        [OperationContract]
+        void AddApplicantWorkHistory(ApplicantWorkHistoryPoco[] pocos);
+        [OperationContract]
+        List<ApplicantWorkHistoryPoco> GetAllAppliccantWorkHistory();
+        [OperationContract]
+        ApplicantWorkHistoryPoco GetSingleApplicantWorkHistory(Guid Id);
+        [OperationContract]
+        void RemoveApplicantWorkHistory(ApplicantWorkHistoryPoco[] pocos);
+        [OperationContract]
+        void UpdateApplicantWorkHIstory(ApplicantWorkHistoryPoco[] pocos);
+
+    }
+}
