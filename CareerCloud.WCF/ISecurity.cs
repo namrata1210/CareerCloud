2eba62adb59e07133f6ea2e566e41b154ccdd92e
jigar <jigar@JIGAR>
2018-08-30 21:38:05 -0400

    create isecurity isystem security system class

--- CareerCloud.WCF/ISecurity.cs
+++ CareerCloud.WCF/ISecurity.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CareerCloud.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,47 @@ namespace CareerCloud.WCF
 {
     interface ISecurity
     {
+        [OperationContract]
+        void AddSecurityLogin(SecurityLoginPoco[] pocos);
+        [OperationContract]
+        List<SecurityLoginPoco> GetAllSecurityLogin();
+        [OperationContract]
+        SecurityLoginPoco GetSingleSecurityLogin(Guid Id);
+        [OperationContract]
+        void RemoveSecurityLogin(SecurityLoginPoco[] pocos);
+        [OperationContract]
+        void UpdateSecurityLogin(SecurityLoginPoco[] pocos);
+        [OperationContract]
+        void AddSecurityLoginsLog(SecurityLoginsLogPoco[] pocos);
+        [OperationContract]
+        List<SecurityLoginsLogPoco> GetAllSecuritysLoginLog();
+        [OperationContract]
+        SecurityLoginsLogPoco GetSingleSecuritysLoginLog(Guid Id);
+        [OperationContract]
+        void RemoveSecurityLoginsLog(SecurityLoginsLogPoco[] pocos);
+        [OperationContract]
+        void UpdateSecurityLoginsLog(SecurityLoginsLogPoco[] pocos);
+        [OperationContract]
+        void AddSecurityLoginsRole(SecurityLoginsRolePoco[] pocos);
+        [OperationContract]
+        List<SecurityLoginsRolePoco> GetAllSecurityLoginsRole();
+        [OperationContract]
+        SecurityLoginsRolePoco GetSingleSecurityLoginsRole(Guid Id);
+        [OperationContract]
+        void RemoveSecurityLoginsRole(SecurityLoginsRolePoco[] pocos);
+        [OperationContract]
+        void UpdateSecurityLoginsRole(SecurityLoginsRolePoco[] pocos);
+        [OperationContract]
+        void AddSecurityRole(SecurityRolePoco[] pocos);
+        [OperationContract]
+        List<SecurityRolePoco> GetSecurityRole();
+        [OperationContract]
+        SecurityRolePoco GetSingleSecurityRole(Guid Id);
+        [OperationContract]
+        void RemoveSecurityRole(SecurityRolePoco[] pocos);
+        [OperationContract]
+        void UpdateSecurityRole(SecurityRolePoco[] pocos);
+           
+            
     }
 }
---
+++ CareerCloud.WCF/System.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CareerCloud.Pocos;
+
+namespace CareerCloud.WCF
+{
+    class System : ISystem
+    {
+        public void AddSystemCountryCode(SystemCountryCodePoco[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSystemLanguageCode(SystemLanguageCodePoco[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SystemCountryCodePoco> GetAllSystemCountryCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SystemLanguageCodePoco> GetAllSystemLanguageCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemCountryCodePoco GetSingleSystemCountryCode(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemLanguageCodePoco GetSingleSystemLanguageCode(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSystemCountryCode(SystemCountryCodePoco[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSystemLanguageCode(SystemLanguageCodePoco[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSystemCountryCode(SystemCountryCodePoco[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSystemLanguageCode(SystemLanguageCodePoco[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
